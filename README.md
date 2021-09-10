abf5e28c64488e83c742f8705843b7bdd6edbf36
animaliads-team <ds.animalia@gmail.com>
2021-09-10 09:21:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ani-textfield
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ani-textfield)
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '@animaliads/css-tokens/global-tokens.css';
+import '@animaliads/animalia-brand/theme.css';
+import '@animaliads/ani-textfield';
+import './style.css';
+
+export default function App() {
+  return <ani-textfield />;
+}
